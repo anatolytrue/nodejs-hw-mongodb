b78548a0d37079dcaf2d76a20de7b985b5c847e1
Anatolii_Tomniak <anatoly.tomnyak@gmail.com>
2024-11-29 20:39:45 +0100

    add step4 - sorting

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,11 +7,19 @@ import {
 } from "../services/contacts.js";
 import createHttpError from "http-errors";
 import { parsePaginationParams } from "../utils/parsePaginationParams.js";
+import { parseSortParams } from "../utils/parseSortParams.js";
 
 
 export const getContactsController = async (req, res) => {
     const { page, perPage } = parsePaginationParams(req.query);
-    const contacts = await getAllContacts({page, perPage});
+    const { sortBy, sortOrder } = parseSortParams(req.query);
+
+    const contacts = await getAllContacts({
+        page,
+        perPage,
+        sortBy,
+        sortOrder
+    });
 
     res.status(200).json({
         status: 200,
